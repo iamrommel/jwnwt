fab22919dbfbc5c64e62bbc8dddbad66563a61dc
Rommel Manalo <manalo.rommel@gmail.com>
2013-03-10 07:48:20 +0800

    Updated the folder lcoation
    Added the English XML only

--- src/Parser/HtmlParser.cs
+++ src/Parser/HtmlParser.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
+using HtmlAgilityPack;
 
 namespace Parser
 {
@@ -13,38 +15,95 @@ namespace Parser
 
         public void Run()
         {
-            var path = @"C:\Personal\GitHub\jwnwt\lib\raw\05_BI12_.GE.xhtml";
-            var stream = new StreamReader(path);
-            XmlTextReader xmlReader = new XmlTextReader(stream);
-            xmlReader.DtdProcessing = DtdProcessing.Ignore;
-            xmlReader.XmlResolver = null;
+            string langauge = @"en\";
+            string sourcePath = @"C:\Personal\GitHub\jwnwt\lib\raw\" + langauge;
+            string destinationPath = @"C:\Personal\Github\jwnwt\lib\html-template\" + langauge;
 
-            var xmlElement = XElement.Load(xmlReader);
 
-            //from el in xmlTree1.Elements()
-            //where((int)el >= 3 && (int)el <= 5)
-            //select el
-                
+            var sourceFileNames = Directory.GetFiles(sourcePath);
+            var totalFilesNames = sourceFileNames.Count();
+            int ctr = 0;
+
+            foreach (var fileName in sourceFileNames)
+            {
+                ctr++;
+
+                Console.WriteLine(string.Format("Doing {0} out of {1}, for file {2}", ctr, totalFilesNames, fileName));
+
+                var sourceFileName = Path.GetFileName(fileName);
+                var destinationFileName = sourceFileName;
+                var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+                if (!fileNameWithoutExtension.Contains("split"))
+                    destinationFileName = string.Format("{0}-split1{1}", fileNameWithoutExtension, Path.GetExtension(fileName));
+
+                UsingXElement(string.Format(@"{0}{1}", sourcePath, sourceFileName), string.Format(@"{0}\{1}", destinationPath, destinationFileName));
+
+
+                //this is is a temporary exit loop
+                //if (ctr >= 50)
+                //    break;
+            }
+
+
+            Console.WriteLine("DONE!!");
+
 
         }
 
-        private void LoadEachFileToMemortyForReading()
+        private static void UsingXElement(string sourcePath, string destinationPath)
         {
+            var xmlReader = new XmlTextReader(sourcePath) { DtdProcessing = DtdProcessing.Ignore, XmlResolver = null };
+            var element = XElement.Load(xmlReader);
 
+            var bodyElement =
+                element.Descendants()
+                       .FirstOrDefault(m => m.Name.LocalName.Equals("body", StringComparison.InvariantCultureIgnoreCase));
+
+            var xml =
+                bodyElement.ToString()
+                           .Replace(@"<body xmlns=""http://www.w3.org/1999/xhtml"">", "<book>")
+                           .Replace("</body>", "</book>");
+
+            var cleanBodyElementt = XElement.Parse(xml);
+
+            //clean up the index file
+            //<p class="st"><a id="page7"></a><b>Genesis</b></p>
+
+            var stParagraph = cleanBodyElementt.Descendants().FirstOrDefault(m => m.Attribute("class") != null && m.Attribute("class").Value == "st");
+            XElement removeAnchor = null;
+            if (stParagraph != null)
+            {
+              
+
+
+
+                //remove the BOLD TAG
+                removeAnchor = stParagraph
+                    .Descendants().FirstOrDefault(s => s != null && s.Name == "a");
+
+                if (removeAnchor != null)
+                {
+                    removeAnchor.Remove();
+                    //after removal reset the value
+
+                    var value = stParagraph.Descendants().FirstOrDefault().Value;
+                    stParagraph.SetValue(value);
+
+                    //update the value fo the class as w_biblebookname
+                    stParagraph.Attribute("class").SetValue("w_biblebookname"); 
+
+                }
+
+            }
+
+
+
+
+            File.WriteAllText(destinationPath, cleanBodyElementt.ToString());
 
 
 
         }
-        /// <summary>
-        /// This will load the file names in the specified folder location
-        /// </summary>
-        /// <returns></returns>
-        public IEnumerable<string> GetFileNames()
-        {
-            throw new NotImplementedException();
-        }
-
-
 
     }
 }
