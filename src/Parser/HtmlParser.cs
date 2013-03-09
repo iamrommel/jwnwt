2deed774a012e06559115525215484f561b42595
Rommel Manalo <manalo.rommel@gmail.com>
2013-03-10 01:03:11 +0800

    Added teh HTML Agilit Pack and VS Solution update

---
+++ src/Parser/HtmlParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser
+{
+    public class HtmlParser 
+    {
+
+        public void Run()
+        {
+            GetFileNames();
+
+            LoadEachFileToMemortyForReading();
+
+
+            
+
+        }
+        /// <summary>
+        /// This will load the file names in the specified folder location
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> GetFileNames()
+        {
+        
+        }
+
+
+
+    }
+}
